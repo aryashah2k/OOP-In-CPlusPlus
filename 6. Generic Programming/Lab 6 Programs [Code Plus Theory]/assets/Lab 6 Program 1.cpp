57a220c54a56b37471def7a9f03ed006043ede61
aryashah2k <arya.shah82@nmims.edu.in>
2021-06-02 23:09:31 +0530

    Add Lab 6 Program 1 Code

---
+++ 6. Generic Programming/Lab 6 Programs [Code Plus Theory]/assets/Lab 6 Program 1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+//CONSTRUCTOR Syntax
+class add
+{
+    int m,n;
+public:
+    add()
+    {
+        int x;
+        m=100;
+        n=50;
+        x=m+n;
+        cout<<x;
+
+    }//constructor defined inside class
+};
+int main()
+{
+  add a;
+}
